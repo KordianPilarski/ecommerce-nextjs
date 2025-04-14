71ce664532ebc574684e2c079d8600294dd2718b
Kordian <kordian.pilarski@gmail.com>
2025-04-14 07:42:33 +0200

    add header and footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Homepage() {
-  return (
-    <>
-      new ecom
-      <Button>ne bvutton</Button>
-    </>
-  );
+  return <></>;
 }
